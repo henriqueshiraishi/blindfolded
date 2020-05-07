7d549573b1b54d3a2dc802239823c74e7192be4a
Henrique <henrique@halkitec.com.br>
2020-05-07 17:39:39 -0300

    Adicionando sessão de elemento inicial e final e dependencias

--- app/models/mqelem.rb
+++ app/models/mqelem.rb
@@ -11,5 +11,7 @@ class MQELEM < ApplicationRecord
   def remove_dependencias
     MQTRAN.where(mqelem_origem_id:  id).delete_all
     MQTRAN.where(mqelem_destino_id: id).delete_all
+    mqdiag.update(mqelem_inicial_id: nil) if mqdiag.mqelem_inicial_id == id
+    mqdiag.update(mqelem_final_id:   nil) if mqdiag.mqelem_final_id   == id
   end
 end
